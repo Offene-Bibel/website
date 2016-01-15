a2bd3fc0d0fab4e2102162972d2208b951a3c30e
Patrick Sebastian Zimmermann <patrick@zakweb.de>
2016-01-15 17:47:29 +0100

    A little work on the create_snapshot.pl script.

--- scripts/create_snapshot.pl
+++ scripts/create_snapshot.pl
@@ -1,83 +1,115 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 use v5.12;
 use strict;
 
 use Getopt::Long;
-use POSIX qw(strftime);
-use File::Slurp;
-use FindBin;
+use POSIX qw( strftime );
+use File::Slurper qw( read_text write_text );
 
-my $drupalSrcFolder = "/kunden/248589_32760/webseiten/ptesting";
-my $wikiSrcFolder = "/kunden/248589_32760/webseiten/wtesting/wiki";
-my $outputFolder = "$FindBin::Bin/..";
-my $outputFile = "snapshot-" . strftime("%Y-%m-%d", localtime) . ".tgz";
-my $dbUser = "db248589_7";
-my $dbName = "db248589_7";
+my @fileList = qw(
+    mediawiki/LocalSettings.php
+    mediawiki/skins/OffeneBibel/OffeneBibel.php
+    mediawiki/extensions/di/iwDrupalConfig.php
+    drupal/sites/default/settings.php
+    drupal/sites/all/modules/mwi/mwi.config
+    static/server-side/headerFooter.php
+);
+my $resourceFolder = "resources";
+my $webFolder = "/kunden/248589_32760/webseiten/offene-bibel.de";
+my $snapshotFolder = "snapshot-" . strftime("%Y-%m-%d", localtime);
+my $outputFile = $snapshotFolder . ".tgz";
+my $dbUser = "db248589_10";
+my $dbName = "db248589_10";
 my $dbHost = "mysql5.offene-bibel.de";
+my $dbPort = "3306";
+my $domain = "offene-bibel.de";
+my $mode = $ARGV[0];
+
+if not $mode ~~ qw( fill, templatify, package ) {
+    die "Wrong action given: \"$mode\" must be one of: fill, templatify, package.";
+}
 
 GetOptions ("source:s" => \$webFolder,
-            "file:s" => \$outputFile,
-            "user:s" => \$dbUser,
-            "name:s" => \$dbName,
-            "host:s" => \$dbHost,
+            "file:s"   => \$outputFile,
+            "user:s"   => \$dbUser,
+            "name:s"   => \$dbName,
+            "host:s"   => \$dbHost,
+            "port:i"   => \$dbPort,
+            "folder:s" => \$snapshotFolder,
+            "domain:s" => \$domain,
             )
             or die("Error in command line arguments\n");
-
-die "Output file already exists" if -e $outputFile;
 
 print "Enter the DB password: ";
 my $dbPassword = <STDIN>; chomp $dbPassword;
 
-
-print 'Creating database dump... ';
-    print `mysqldump -u$dbUser -p$dbPassword -h$dbHost $dbName -r$outputFolder/mysqldump.bin`;
-say 'done';
-
-print 'Copying drupal... ';
-    print `rm -r $outputFolder/drupal`;
-    print `cp -r $drupalSrcFolder $outputFolder/drupal`;
-say 'done';
-
-print 'Copying mediawiki... ';
-    print `rm -r $outputFolder/mediawiki`;
-    print `cp -r $mediawikiSrcFolder $outputFolder/mediawiki`;
-say 'done';
-
-print 'Replacing database credentials... ';
-    my @fileList = qw(
-    mediawiki/LocalSettings.php
-    drupal/sites/default/settings.php
-    );
-
-    for (@fileList) {
-        my $file = "$outputFolder/$_";
-        my @lines = read_file($file);
-        chmod 0660, $file;
-        open (OUT, ">", $file) or die $!;
-        for (@lines) {
-            if m/^(.*)\/\/<<&REPLACEMENT&>>(.*)$/ {
-                print "$2//<<&REPLACEMENT&>>$2";
-            }
-
-            s/$dbPassword/<&db_password&>/g;
-            s/$dbName/<&db_name&>/g;
-            s/$dbUser/<&db_user&>/g;
-            s/$dbHost/<&db_host&>/g;
-            s/$dbHost/<&db_host_port&>/g;
-            print OUT; 
+if ( $mode eq 'fill' ) {
+    print 'Filling in database credentials... ';
+        for (@fileList) {
+            my $file = "$snapshotFolder/offene-bibel.de/$_";
+            my $template_file = $file . "_TEMPLATE";
+            my $content = read_text( $template_file );
+            $content =~ s/<&db_password&>/$dbPassword/g;
+            $content =~ s/<&db_name&>/$dbName/g;
+            $content =~ s/<&db_user&>/$dbUser/g;
+            $content =~ s/<&db_host&>/$dbHost/g;
+            $content =~ s/<&db_port&>/$dbPort/g;
+            $content =~ s/<&domain&>/$domain/g;
+            chmod 0660, $file;
+            write_text( $file, $content );
         }
-        close OUT;
+    say 'done';
+}
+elsif ( $mode eq 'templatify' ) {
+    for (@fileList) {
+        my $file = "$snapshotFolder/offene-bibel.de/$_";
+        my $template_file = $file . "_TEMPLATE";
+        my $content = read_text( $file );
+        $content =~ s/$dbPassword/<&db_password&>/g;
+        $content =~ s/$dbName/<&db_name&>/g;
+        $content =~ s/$dbUser/<&db_user&>/g;
+        $content =~ s/$dbHost/<&db_host&>/g;
+        $content =~ s/$dbPort/<&db_port&>/g;
+        $content =~ s/(?<!\@)$domain/<&domain&>/g;
+        chmod 0660, $template_file;
+        write_text( $template_file, $content );
     }
-say 'done';
+}
+elsif ( $mode eq 'package' ) {
+    die "Output folder path already exists" if -e $snapshotFolder;
+    die "Output file already exists" if -e $outputFile;
 
-print 'Zipping... ';
-    print `tar -czf $outputFile $outputFolder`;
-say 'done';
+    mkdir $snapshotFolder;
 
-print 'Removing temporary files... ';
-    print `chmod -R u+w $outputFolder`;
-    print `rm -rf $outputFolder`;
-say 'done';
+    print 'Creating database dump... ';
+        print `mysqldump -u$dbUser -p'$dbPassword' -h$dbHost $dbName -r$snapshotFolder/mysqldump.bin`;
+    say 'done';
 
-say 'Finished.';
+    print 'Copying files... ';
+        print `cp -r $webFolder $snapshotFolder/offene-bibel.de`;
+    say 'done';
+
+    # copy supplementary files (documentation, helper scripts, ...)
+    print 'Copying supplementary files... ';
+        print `cp $resourceFolder/* $snapshotFolder`;
+    say 'done';
+
+    print 'Removing templated files... ';
+    for (@fileList) {
+        my $file = "$snapshotFolder/offene-bibel.de/$_";
+        unlink $file;
+    }
+    say 'done';
+
+    print 'Zipping... ';
+        print `tar -czf $outputFile $snapshotFolder`;
+    say 'done';
+
+    print 'Removing temporary files... ';
+        print `chmod -R u+w $snapshotFolder`;
+        print `rm -rf $snapshotFolder`;
+    say 'done';
+
+    say 'Finished.';
+}
 
